f4824ffe98a10f2603bd653b80da7556f3692471
barisozasik-pixel <barisozasik@gmail.com>
2026-09-08 19:29:57 +0300

    Update 14_savas_botu.js

--- js_modulleri/14_savas_botu.js
+++ js_modulleri/14_savas_botu.js
@@ -1,10 +1,11 @@
 // js_modulleri/14_savas_botu.js
 
 // DİKKAT: Bu API anahtarı sadece test amaçlıdır. Canlıya alırken Supabase Edge Functions'a taşınmalıdır.
-const GEMINI_API_KEY = "AQ.Ab8RN6JkzqvOWl9jucgvfZEDMDhCQiUz1b2hzLQ0vlzyu1qXgw";
+const GEMINI_API_KEY = "AQ.Ab8RN6IiXEhxkf5-Eg3X_iwI78PVt3paqwt9XiuCXp1-1iLvcQ";
 
 async function callGeminiAPI(promptText) {
-    if (!GEMINI_API_KEY === "AQ.Ab8RN6JkzqvOWl9jucgvfZEDMDhCQiUz1b2hzLQ0vlzyu1qXgw") {
+    if (!GEMINI_API_KEY === "AQ.Ab8RN6IiXEhxkf5-Eg3X_iwI78PVt3paqwt9XiuCXp1-1iLvcQ") 
+    {
         console.error("Lütfen 14_savas_botu.js dosyasındaki GEMINI_API_KEY değişkenine kendi API anahtarınızı yapıştırın.");
         return null;
     }
